fab445dd2ec9e43faedddbfa9c7e5767227430b0
Joonas Teller <jteller@hkhk.edu.ee>
2025-11-10 10:33:27 +0200

    Esimene fail

---
+++ h01.py
@@ -0,0 +1,42 @@
+# Joonas Teller
+# Harjutus 01
+# print("Tere \"suur\" maailm!")
+# Kolmnurk
+import turtle
+turtle.speed(0)
+turtle.pu()
+turtle.goto(-400,300)
+turtle.pd()
+for i in range(3):
+    turtle.forward(200)
+    turtle.right(120)
+
+turtle.pu()
+turtle.goto(-100,300)
+turtle.pd()
+
+turtle.forward(35)
+turtle.right(90)
+turtle.forward(190)
+turtle.right(45)
+turtle.forward(45)
+turtle.right(45)
+turtle.forward(50)
+turtle.right(45)
+turtle.forward(45)
+turtle.right(45)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(35)
+turtle.right(90)
+turtle.forward(15)
+turtle.left(45)
+turtle.forward(20)
+turtle.left(45)
+turtle.forward(15)
+turtle.left(45)
+turtle.forward(20)
+turtle.left(45)
+turtle.forward(190)
+
+
