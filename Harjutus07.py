022eb8b98bbd2d31531f43335e9799fe8ba83c75
Joonas Teller <jteller@hkhk.edu.ee>
2025-12-03 09:20:35 +0200

    Python

--- Harjutus07.py
+++ Harjutus07.py
@@ -1,5 +1,16 @@
 #03.12 Joonas
 
 
-nimede_loend = ["Alice", "Bob", "Carol", "Dave", "Eve"]
-print(nimede_loend[2])
+playlist = ['1. ALIKA – "Bridges"',
+            '2. Anett x Fredi – "Read Between The Lines"',
+            '3. villemdrillem – "leekiv armastus"',
+            '4. Clicherik & Mäx – "PAKSUD"',
+            '5. nublu – "ära ärata"',
+            '6. NOËP – "Move Your Feet"',
+            '7. Trad.Attack! – "Bring It On"',
+            '8. Bedwetters – "It Is What It Is"',
+            '9. Reket – "Panama paberid"',
+            '10. Grete Paia – "Võluväel"']
+
+valik = int(input("Vali ligu: "))
+print(playlist[valik-1])
